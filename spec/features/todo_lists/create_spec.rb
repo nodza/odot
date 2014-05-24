6abac8b10d1434234b40fc0c6e44a630cb15e033
Noel Hwande <noel.hwande@gmail.com>
2014-05-24 15:01:46 -0500

    Add length validation to todo list title

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -31,4 +31,22 @@ describe "Creating todo lists" do
     expect(page).to_not have_content("This is how we do it")
   end
 
+  it "displays an error when the todo list has a title of less than 3 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Hi"
+    fill_in "Description", with: "My list of todos"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is how we do it")
+  end
+
 end
